0f0b3252b9f94c892676d4a184dfa6f6d3425f52
Dario Zubaray <dario.zubaray@repairsmith.com>
2022-09-11 13:37:36 -0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { env } from 'process';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,7 +12,16 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true
     })
-  )
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo Restfull Api')
+    .setDescription('Teslo shop endpoints')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
